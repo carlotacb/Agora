6d661a533775b94bff0f12f0438279c1a99c8703
Carlota <carkbra@gmail.com>
2017-10-20 12:33:12 +0200

    Connect login with DB

--- cliente/Agora/app/src/main/java/edu/upc/pes/agora/LoginActivity.java
+++ cliente/Agora/app/src/main/java/edu/upc/pes/agora/LoginActivity.java
@@ -3,6 +3,10 @@ package edu.upc.pes.agora;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
 
@@ -25,43 +29,101 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity {
 
     private Button login, register;
     private EditText etUsername, etPassword;
+    private String URI = "https://dragos.ngrok.io";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login = (Button)findViewById(R.id.btnLogin);
+        login = (Button) findViewById(R.id.btnLogin);
         register = (Button) findViewById(R.id.btnRegister);
 
-        etUsername = (EditText)findViewById(R.id.username);
-        etPassword = (EditText)findViewById(R.id.password);
+        etUsername = (EditText) findViewById(R.id.username);
+        etPassword = (EditText) findViewById(R.id.password);
 
-        login.setOnClickListener(this);
-        register.setOnClickListener(this);
+
+        login.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = etUsername.getText() + "";
+                String password = etPassword.getText() + "";
+
+                Context con = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+                // TODO: PONER EL MULTIIDIOMA
+                CharSequence textusercas = "Porfavor introduce un usuario válido";
+                CharSequence textpascas = "Contraseña incorrecta";
+
+                if (username.length() == 0) {
+                    Toast toast = Toast.makeText(con, textusercas, duration);
+                    toast.show();
+                }
+
+                if(password.length() == 0) {
+                    Toast toast = Toast.makeText(con, textpascas, duration);
+                    toast.show();
+                }
+                
+            }
+        });
+
+        register.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
 
     }
 
+    public class Networking extends AsyncTask {
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-            case R.id.btnLogin:
-                break;
-            case R.id.btnRegister:
+        public static final int NETWORK_STATE_REGISTER=1;
+        @Override
+        protected Object doInBackground(Object[] params) {
+
+            getJson(URI, NETWORK_STATE_REGISTER);
+            return null;
+        }
+    }
+
+    private void getJson(String url, int state) {
+
+        HttpClient httpClient = new DefaultHttpClient();
+        HttpPost request = new HttpPost(url);
+        List<NameValuePair> postParameters = new ArrayList<NameValuePais>();
+
+        switch(state) {
+            case Networking.NETWORK_STATE_REGISTER:
+
+                postParameters.get(new BasicNameValuePair("username", etUsername));
+
+
                 break;
             default:
                 break;
         }
+
     }
 }
 
