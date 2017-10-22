58604dbdf242b783743a53b5de2b13c14955a72e
Kerstin <winterke@students.uni-marburg.de>
2017-10-22 13:53:00 +0200

    Fixed error in registration form

--- cliente/Agora/app/src/main/java/edu/upc/pes/agora/RegisterActivity.java
+++ cliente/Agora/app/src/main/java/edu/upc/pes/agora/RegisterActivity.java
@@ -1,34 +1,20 @@
 package edu.upc.pes.agora;
 
-import android.content.ContentValues;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import org.apache.http.HttpResponse;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText identificador = (EditText) findViewById(R.id.identificador);
-    EditText username = (EditText) findViewById(R.id.username);
-    EditText password1 = (EditText) findViewById(R.id.password1);
-    EditText password2 = (EditText) findViewById(R.id.password2);
+    EditText identifier;
+    EditText username;
+    EditText password1;
+    EditText password2;
     String user;
     String id;
     String pw1;
@@ -38,13 +24,19 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        identifier = (EditText) findViewById(R.id.identifier);
+        username = (EditText) findViewById(R.id.username);
+        password1 = (EditText) findViewById(R.id.password1);
+        password2 = (EditText) findViewById(R.id.password2);
+
     }
 
-    public void onClick(View v) throws IOException {
+    public void onClick(View v) throws Exception {
         Intent button = new Intent();
         switch (v.getId()){
             case R.id.btnRegistration:
-                id = identificador.getText().toString();
+                id = identifier.getText().toString();
                 user = username.getText().toString();
                 pw1 = password1.getText().toString();
                 pw2 = password2.getText().toString();
@@ -60,7 +52,7 @@ public class RegisterActivity extends AppCompatActivity {
                     Toast.makeText(this.getApplicationContext(),"Passwords must be the same.", Toast.LENGTH_SHORT).show();
                 }
         }
-        startActivity(button);
+        startActivity(new Intent (this, RegisterActivity.class));
     }
 
 
