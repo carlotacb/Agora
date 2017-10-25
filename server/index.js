b70e4ef9bbab741f5a5b733b0552658457d840c5
Drag0s <me@dragosfotescu.com>
2017-10-26 00:09:39 +0200

    Add morgan

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 const config = require('./config')
 
@@ -13,6 +14,7 @@ StartServer(app)
 
 function BootstrapServer(app) {
     app.use(bodyParser.json())
+    app.use(morgan('combined'))
 }
 
 function StartServer(app) {
